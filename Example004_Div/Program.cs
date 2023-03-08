e3849761675f8a82ae0b9fe328607073a405e99e
Ivan <kiseleff2010@yandex.ru>
2023-03-08 15:10:01 +0300

    Добавлена инстуркция для random

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -4,3 +4,5 @@ double numberB = new Random().Next(1,10);
 Console.WriteLine(numberA);
 Console.WriteLine(numberB);
 Console.WriteLine(numberA / numberB);
+//new Random().Next(min, max) - создает случайное число в указанном диапозоне, причем max будет на 1 меньше, то есть если укажем 10, то будет число 9
+// Диапазон можно задать по разному [min, max-1][min, max)
