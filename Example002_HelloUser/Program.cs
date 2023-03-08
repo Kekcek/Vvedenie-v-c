2cedc2a8ea6632f1b3b9d695237f93287e440b22
Ivan <kiseleff2010@yandex.ru>
2023-03-08 15:11:42 +0300

    Добавлена инстуркци

--- Example002_HelloUser/Program.cs
+++ Example002_HelloUser/Program.cs
@@ -4,3 +4,7 @@ string username = Console.ReadLine();
 
 Console.Write("Привет, ");
 Console.Write(username);
+
+// Write(); - вывод в одну строку
+// WriteLine(); - В конце перейти на новую строчку
+// ReadLine(); - Считать строку из терминала
